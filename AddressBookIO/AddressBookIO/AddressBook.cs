0426a92dae43cbfc585dc93198beae0fecc47a05
snehasonwane <snehasonwane01@gmail.com>
2022-05-31 21:46:03 +0530

    [Manasi]Add :Ability To Sort Entries alphabetically

---
+++ AddressBookIO/AddressBookIO/AddressBook.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookIO
+{
+      class AddressBook
+    {
+        List<Person> adressBookList;//create list
+        public AddressBook()//default constructor
+        {
+            this.adressBookList = new List<Person>();
+        }
+        public void AddContact(string firstName, string lastName, string address, string city, string state, long phoneNumber, string email, int zip)
+        {
+            bool flag = this.adressBookList.Any(item => item.firstName == firstName && item.lastName == lastName);
+            if (!flag)
+            {
+                Person person = new Person(firstName, lastName, city, state, email, phoneNumber, zip);//create new object of person class
+                adressBookList.Add(person);//adding person details in addressbookList 
+                Console.WriteLine("Contact added Successfully");
+                Console.WriteLine();
+                Console.WriteLine("New contact");
+            }
+            else
+            {
+                Console.WriteLine("{0}{1} this contact already exist in Address Book :", firstName, lastName);
+            }
+        }
+    }
+}
---
+++ AddressBookIO/AddressBookIO/FileReadWrite.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookIO
+{
+     class FileReadWrite
+    {
+        static String FilePath = @"C:\Users\Radha\source\repos\AddressBookSystem\AddressBookSystem\Address.txt";
+        static string FilePathCsv = @"C:\Users\Radha\source\repos\AddressBookSystem\AddressBookSystem\Csvdata.csv";
+        static String filePathJson = @"C:\Users\Radha\source\repos\AddressBookSystem\AddressBookSystem\jsonfile.json";
+        public static void WriteTxtFile(List<Person> persons)
+        {
+            if (File.Exists(FilePath))
+            {
+                using (StreamWriter streamWriter = File.AppendText(FilePath))
+                {
+                    foreach (Person person in persons)
+                    {
+                        streamWriter.WriteLine(" \nPersons detail ");
+                        streamWriter.WriteLine("FirstName: " + person.firstName);
+                        streamWriter.WriteLine("LastName: " + person.lastName);
+                        streamWriter.WriteLine("City    : " + person.city);
+                        streamWriter.WriteLine("Email   : " + person.email);
+                        streamWriter.WriteLine("State   : " + person.state);
+                        streamWriter.WriteLine("PhoneNum: " + person.phoneNumber);
+
+                    }
+                    streamWriter.Close();
+                }
+                Console.WriteLine("Writting Persons detail in to the Text the file");
+            }
+            else
+            {
+                Console.WriteLine("No such file exists");
+            }
+        }
+    }
+}
